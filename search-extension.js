7b19fea9163e514f5c32201938a0279d8bbba149
Shakeel Mohamed <smohamed@splunk.com>
2016-03-02 01:29:38 -0800

    Start work on #16

--- search-extension.js
+++ search-extension.js
@@ -7,11 +7,13 @@ chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
 });
 
 function navigate(url) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.update(tabs[0].id, {url: url});
-  });
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.update(tabs[0].id, {url: url});
+    });
 }
 
 chrome.omnibox.onInputEntered.addListener(function(text) {
-    navigate("https://zenplayer.audio?v=" + encodeURIComponent(text) )
+    // TODO: lame hack, assume we're doing real search if we find a space
+    var param = text.indexOf(" ") ? "q=" : "v=";
+    navigate("https://zenplayer.audio?" + param + encodeURIComponent(text));
 });
